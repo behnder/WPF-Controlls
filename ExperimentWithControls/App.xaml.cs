7a5e1fca87a4011e318b55f9dc9bf641b078415b
Nicolas Mercado <mercado.nicolas@live.com>
2021-09-24 13:13:55 -0300

    first commit

---
+++ ExperimentWithControls/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExperimentWithControls_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
